b83b0b5573ca197e8cd523a6ca327df556be0964
Davi F <davi.felix.lima@gmail.com>
2022-02-09 15:41:48 -0300

    feature: Home design ready

---
+++ src/components/SubscribeButton.tsx
@@ -0,0 +1,10 @@
+
+import styles from '../styles/SubscribeButton.module.scss'
+
+export function SubscribeButton() {
+  return (
+    <button className={styles.button}>
+      Subscribe Now
+    </button>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,7 @@
 import Head from 'next/head'
+import { SubscribeButton } from '../components/SubscribeButton'
+
+import styles from '../styles/Home.module.scss'
 
 export default function Home() {
   return (
@@ -6,7 +9,15 @@ export default function Home() {
       <Head>
         <title>Home | ig.news</title>
       </Head>
-      <main></main>
+      <main className={styles.main}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>News about the <br /> <span>React</span> world</h1>
+          <p>Get access to all publication <br /> <span>for $9.90 / month</span></p>
+          <SubscribeButton />
+        </section>
+        <img src="/woman.svg" alt="Woman coding in React" />
+      </main>
     </>
   )
 }
