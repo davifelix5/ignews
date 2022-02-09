fe8144b7a83f643a8a3bbd447366a56b7ef85edf
Davi F <davi.felix.lima@gmail.com>
2022-02-09 11:42:09 -0300

    feature: configure _document.ts and global style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,15 @@
+import Head from 'next/head'
 import styles from '../styles/Home.module.scss'
 
 export default function Home() {
   return (
-    <h1 className={styles.title}>
-      Hello world
-    </h1>
+    <>
+      <Head>
+        <title>Home | ig.news</title>
+      </Head>
+      <h1 className={styles.title}>
+        Hello world
+      </h1>
+    </>
   )
 }
