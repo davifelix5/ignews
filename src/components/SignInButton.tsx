094a6ac0e35fb4b6c2dec21d8273f97f2e1b69bb
Davi F <davi.felix.lima@gmail.com>
2022-02-10 10:36:10 -0300

    feature: github oauth with next-auth

--- src/components/SignInButton.tsx
+++ src/components/SignInButton.tsx
@@ -1,19 +1,36 @@
 import styles from '../styles/SignInButton.module.scss'
 
+import { signIn, signOut, useSession } from 'next-auth/react'
+
 import { FaGithub } from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
 
 export function SignInButton() {
-  const isUserLoggedIn = false
+  const { status, data } = useSession()
+
+  const isUserLoggedIn = status === 'authenticated'
+
+  function handleAuthenticateClientWithGithub() {
+    signIn('github')
+  }
+
+  function handleUserSignOff() {
+    signOut()
+  }
 
   return isUserLoggedIn ? (
-    <button className={`${styles.button} ${styles.loggedIn}`}>
+    <div className={`${styles.button} ${styles.loggedIn}`}>
       <FaGithub size={25} color="#04D361" />
-      <span>davifelix5</span>
-      <FiX size={20} color="#a8a8b3" />
-    </button>
+      <span>{ data.user.name }</span>
+      <button onClick={handleUserSignOff}>
+        <FiX size={20} color="#a8a8b3" />
+      </button>
+    </div>
   ) : (
-    <button className={`${styles.button} ${styles.loggedOff}`}>
+    <button 
+      className={`${styles.button} ${styles.loggedOff}`} 
+      onClick={handleAuthenticateClientWithGithub}
+    >
       <FaGithub size={25} color="#eba417" />
       <span>Sign in with GitHub</span>
     </button>
