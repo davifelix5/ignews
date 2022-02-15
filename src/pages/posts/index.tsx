e6f7235ff57ef88d69b1d8076b6d1cd973de0d12
Davi Felix <davi.felix.lima@gmail.com>
2022-02-15 11:37:11 -0300

    feature: listing posts from prismic into the Posts page

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,12 +1,24 @@
+import { GetStaticProps } from 'next'
 import Head from 'next/head'
 
 import styles from '../../styles/Posts.module.scss'
 
 import * as prismic from '@prismicio/client'
 import { createPrismicClient } from '../../services/prismic'
-import { GetStaticProps } from 'next'
+import { RichText } from 'prismic-dom'
 
-export default function Posts({ posts }) {
+interface Post {
+	slug: string
+	title: string
+	summary: string
+	updatedAt: string
+}
+
+interface PostsProps {
+	posts: Post[]
+}
+
+export default function Posts({ posts }: PostsProps) {
 	return (
 		<>
 			<Head>
@@ -14,27 +26,15 @@ export default function Posts({ posts }) {
 			</Head>
 			<section className={styles.container}>
 				<ul>
-					<li>
-						<a href="">
-							<time>12/03/2022</time>
-							<h2>Creating a Monorepo with Lerna {"&"} Yarn Workspaces</h2>
-							<p>In this guide, you will learn how to create a Monorepo to manage multiple packages with a shared build, test, and release process.</p>
-						</a>
-					</li>
-					<li>
-						<a href="#">
-							<time>08/03/2022</time>
-							<h2>How Stripe Designs Beautiful Websites</h2>
-							<p>Examining the tips and tricks used to make Stripe's website design a notch above the rest.</p>
-						</a>
-					</li>
-					<li>
-						<a href="">
-							<time>04/03/2022</time>
-							<h2>Past, Present, and Future of React State Management</h2>
-							<p>Learn about the history of state management in React and what the preferred solutions are today.</p>
-						</a>
-					</li>
+					{posts.map(post => (
+						<li>
+							<a href="">
+								<time>{post.updatedAt}</time>
+								<h2>{post.title}</h2>
+								<summary>{post.title}</summary>
+							</a>
+						</li>
+					))}
 				</ul>
 			</section>
 		</>
@@ -50,9 +50,22 @@ export const getStaticProps: GetStaticProps = async (req) => {
 		pageSize: 100,
 	})
 
+	const posts = response.results.map(post => {
+		return {
+			slug: post.uid,
+			title: RichText.asText(post.data.title),
+			summary: post.data.content.find(content => content.type === 'paragraph')?.text || '',
+			updatedAt: new Date(post.last_publication_date).toLocaleDateString('en-US', {
+				day: '2-digit',
+				month: 'long',
+				year: 'numeric'
+			})
+		}
+	})
+
 	return {
 		props: {
-			posts: response.results
+			posts
 		},
 		revalidate: 60 * 30 // 30 minutes
 	}
